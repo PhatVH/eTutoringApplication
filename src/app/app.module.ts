bf2d6ebd2d2fa1011de99943c33670435c2b104a
NgoVietAnh1998 <adamsimons911@gmail.com>
2020-04-01 22:38:34 +0700

    commit
    ThirdCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListStudentComponent } from './list-student/list-student.component';
+import { ListBlogComponent } from './list-blog/list-blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListStudentComponent,
+    ListBlogComponent
   ],
   imports: [
     BrowserModule
